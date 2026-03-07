d8372ff0f8d195ae1105fa799b71e97e27f5f98e
Rigo <217056768+Recsaur@users.noreply.github.com>
2026-03-07 23:22:59 +0400

    Added 4 different zombies types. Made simple AI for the zombies, adding roaming, attacking, and chase. Gave player mop weapon and made zombies have detection of which weapon is being used to attack them.

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -3,6 +3,9 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -800.0
 
+func _ready() -> void:
+	HitSignal.hit.connect(TakeDmg)
+
 func _input(event: InputEvent) -> void:
 	if $Mop/Area2D/CollisionShape2D.disabled == true:
 		if event.is_action_pressed("Attack"):
@@ -14,18 +17,15 @@ func _input(event: InputEvent) -> void:
 
 
 func _physics_process(delta: float) -> void:
+	var direction := Input.get_axis("Left", "Right")
+	
+	if GameControl.JanitorHealth <= 0:
+		queue_free()
 	GameControl.JanitorPosX = global_position.x
-	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-		
-	# Handle jump.
 	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("Left", "Right")
 	if direction:
 		if direction == -1:
 			$Sprite2D.flip_h = true
@@ -35,5 +35,16 @@ func _physics_process(delta: float) -> void:
 			velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
 	move_and_slide()
+
+
+func TakeDmg(Dmg,EnemyPos_x):
+	var Knockback_x = 2000
+	var Knockback_y = 250
+	
+	GameControl.JanitorHealth -= Dmg
+	if EnemyPos_x > GameControl.JanitorPosX:
+		velocity.x = -Knockback_x
+	else:
+		velocity.x = Knockback_x
+	velocity.y = -Knockback_y
---
+++ z_buff.gd
@@ -0,0 +1,67 @@
+extends CharacterBody2D
+
+const JUMP_VELOCITY = -200.0
+const Dmg = 2
+const SPEED = 125.0
+
+var Health = 150
+var found_janitor = false
+var direction = 1
+
+
+func add_gravity(delta):
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+func move_enemy():
+	velocity.x = SPEED * direction
+
+func reverse_direction():
+	if is_on_wall():
+		direction = -direction
+		$AnimatedSprite2D.flip_h = true
+
+func _physics_process(delta: float) -> void:
+	if Health <=0:
+		queue_free()
+	add_gravity(delta)
+	move_and_slide()
+	if found_janitor == false:
+		move_enemy()
+		reverse_direction()
+	else:
+		if GameControl.JanitorPosX > global_position.x:
+			$AnimatedSprite2D.flip_h = false
+			direction = 1
+		elif GameControl.JanitorPosX < global_position.x:
+			$AnimatedSprite2D.flip_h = true
+			direction = -1
+		velocity.x = SPEED * direction
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	found_janitor = true
+	if area.is_in_group("Mop"):
+		print("is a mop")
+		Takedmg(25,250,0.5)
+	if area.is_in_group("Shiv"):
+		print("is a shiv")
+		Takedmg(25,250,0.25)
+
+func Takedmg(HealthLoss,Knockback,HitSpeed):
+	if $Timer.is_stopped():
+		velocity.y = JUMP_VELOCITY
+		Health -= HealthLoss
+		print("AHH HIT")
+		print(Health)
+		$Timer.start(HitSpeed)
+		if GameControl.JanitorPosX > global_position.x:
+			velocity.x = -Knockback
+		else:
+			velocity.x = Knockback
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.is_in_group("janitor"):
+		HitSignal.hit.emit(Dmg,position.x)
+		found_janitor = true
+		if owner.has_method("TakeDmg"):
+			owner.take_damage(Dmg,position.x)
